5509719b4d03a33f845180edab2a1e4dc5697767
Jose Andrey <134113125+J-Andrey@users.noreply.github.com>
2026-08-06 22:51:27 -0300

    update

--- services/pagseguroService.js
+++ services/pagseguroService.js
@@ -43,15 +43,14 @@ async function criarCheckout(pedido) {
             console.log("===== REQUEST PAGBANK =====");
             console.log(JSON.stringify(payload, null, 2));
 
-            const { data } = await client.post("/checkouts", payload);
+                const { data } = await client.post(
+                    "/checkouts",
+                    payload
+                );
 
             console.log("===== RESPONSE PAGBANK =====");
             console.log(JSON.stringify(data, null, 2));
 
-        const { data } = await client.post(
-            "/checkouts",
-            payload
-        );
 
         return normalizarCheckout(data);
     } catch (error) {
